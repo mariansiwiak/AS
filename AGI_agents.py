b901656a7f88d4df62ff67399414a582354736c4
Marian Siwiak, Phd <129772661+mariansiwiak@users.noreply.github.com>
2023-11-24 18:50:16 +0100

    Add files via upload

--- AGI_agents.py
+++ AGI_agents.py
@@ -8,14 +8,23 @@ class HumanInteraction:
     async def run(self, llm, input):
         print(f"This is input: {input}.")
         prompt_template = PromptTemplate.from_template("Could you please answer this: {input}?")
-        chain = prompt_template | llm
-        result = chain.invoke({'input': input})
-        print(f"This is the answer: {result}.")
+        #chain = prompt_template | llm
+        #result = chain.invoke({'input': input})
+        #print(f"This is the answer: {result}.")
+        print(f"Input unprocessed: {input}.")
 
-class Contemplation:
+class DefaultModeNetwork:
     def __init__(self):
-        self.description = "Self-contemplation placeholer"
+        self.description = "Intropsective review placeholder"
 
     async def run(self):
         #print("Contemplating...")
-        await asyncio.sleep(10)  # Simulate contemplation
+        await asyncio.sleep(3)  # Simulate contemplation
+
+class DeepSleep:
+    def __init__(self):
+        self.description = "Self-fine-tuning placeholder"
+
+    async def sleep(self):
+        print("Zzzzzz...")
+        await asyncio.sleep(5)  # Simulate some work
